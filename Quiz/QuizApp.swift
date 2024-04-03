48f34d395f3c3715ea6ddfc79c44f6d603385097
kota-aso <kotaaso1212@gmail.com>
2024-04-03 20:41:43 +0900

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Kota Aso on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
